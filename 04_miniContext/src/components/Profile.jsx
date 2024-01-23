867ac769c9d3c81f825abb00eda9569ee579caf7
Sameer Choudhary <csameer714@gmail.com>
2024-01-24 00:20:05 +0530

    context api project

---
+++ 04_miniContext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+    const {User} = useContext(UserContext);
+
+    if(!User) return <div>Please Login!</div>
+
+    return <div>Welcome {User.username}</div>
+}
+
+export default Profile
