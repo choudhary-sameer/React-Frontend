55dcedbe23f779a3c10a5967be811f9e6cdc7f51
Sameer Choudhary <csameer714@gmail.com>
2024-01-31 21:28:29 +0530

    configuring redux toolkit

--- 08_MegaBlog/src/App.jsx
+++ 08_MegaBlog/src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice";
+import "./App.css";
+import { Header, Footer } from "./components/index";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Blog App with Appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        userData ? dispatch(login({ userData })) : dispatch(logout(userData));
+      }) // include .catch here
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flew-wrap content-between bg-blue-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* { <Outlet /> } */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
---
+++ 08_MegaBlog/src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./header/Header"
+import Footer from "./Footer/Footer"
+
+export { Header,
+         Footer }
